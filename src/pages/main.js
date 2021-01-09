c5bd5d535be6bbb1587359ef46b6ef73e037201e
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2021-01-08 21:36:46 -0300

    update

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,30 +1,25 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable quotes */
 import React, { Component } from "react";
 
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default class Main extends Component {
-  static navigationOptions = {
-    title: "Envelhecimento Ativo"
-  };
-
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Este aplicativo tem a finalidade de avaliar o tipo de envelhecimento
-          da população. Caso tenha interesse de participar da pesquisa, clique
-          no botão abaixo.
+          Vamos testar como você está envelhecendo?
         </Text>
         <TouchableOpacity
           style={styles.loginButton}
           onPress={() => {
-            this.props.navigation.navigate("Basic");
+            this.props.navigation.navigate("Quest");
           }}
         >
           <Text style={styles.loginButtonText}>Iniciar teste</Text>
         </TouchableOpacity>
+
+        <Text style={styles.score}>Score</Text>
+        <Text style={styles.score}>0%</Text>
       </View>
     );
   }
@@ -34,24 +29,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-
     backgroundColor: "#fafafa"
   },
 
   welcome: {
-    fontSize: 18,
+    fontSize: 20,
     textAlign: "center",
     margin: 10,
-    fontFamily: "Roboto-Regular",
     fontWeight: "bold",
-    padding: 20
+    padding: 30
   },
 
   score: {
     fontSize: 30,
     textAlign: "center",
     margin: 10,
-    fontFamily: "Roboto-Regular",
     color: "rgba(0, 0, 0, 0.5)",
     fontWeight: "bold"
   },
@@ -60,7 +52,7 @@ const styles = StyleSheet.create({
     height: 42,
     borderRadius: 5,
     borderWidth: 2,
-    borderColor: "#ef5350",
+    borderColor: "#DA552F",
     backgroundColor: "transparent",
     justifyContent: "center",
     alignItems: "center",
@@ -69,7 +61,7 @@ const styles = StyleSheet.create({
 
   loginButtonText: {
     fontSize: 16,
-    color: "#ef5350",
+    color: "#DA552F",
     fontWeight: "bold"
   }
 });
